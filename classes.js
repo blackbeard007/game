0bb324ebf5038dedc3161303a9285b2fca0d6271
blackbeard007 <soured1996@gmail.com>
2017-05-04 12:36:28 +0300

    add class Warrior,add method defend to Enemy and Kingdom,add func isAlive,getHealth,getPower add button and its listener

--- classes.js
+++ classes.js
@@ -1,51 +1,118 @@
 /* author Vlad Yakymenko Dp-P05 JS Core */
 'use strict';
-var battle = new Battle(),
-    kingdom = new Kingdom(),
-    enemy = new Enemy();
 
-battle.fight();
+window.onload = function () {
+
+    function isAlive (health) {
+        return health > 0;
+    }
+
+    function getHealth (arr) {
+        let result = 0;
+
+        arr.forEach(function (item) {
+            result += item.health;
+        });
+
+        return result;
+    }
+
+    function getPower (arr) {
+        let result = 0;
+
+        arr.forEach(function (item) {
+            result += item.power;
+        });
+
+        return result;
+    }
 /* ----------Класс Battle----------- */
-function Battle () {
-    Battle.prototype.fight = function () {
-        for (let i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                kingdom.attack();
-            } else {
-                enemy.attack();
-            }
-        }
-    };
+    class Battle {
 
-    return this;
-}
+        fight() {
+            console.log("Power of kingdom %i", kingdom.power);
+            console.log("Power of enemy %i", enemy.power);
+            while (1) {
+
+                if (isAlive(kingdom.health)) {
+                    kingdom.attack();
+                    enemy.defend(kingdom.power);
+                } else {
+                    console.log('!!!!Enemy won!!!!');
+                    break;
+                }
+
+                if (isAlive(enemy.health)) {
+                    enemy.attack();
+                    kingdom.defend(enemy.power);
+                } else {
+                    console.log('!!!!Kingdom won!!!!');
+                    break;
+                }
+
+            }
+                         
+        }
+
+    }
 
 /* ----------------------------------- */
 
 /*--------- Класс Kingdom------------- */
-function Kingdom () {
-    let power;
+    class Kingdom {
+        constructor(health, power) {
+            let warriors = [new Warrior('Gnome-Grisha'),
+                             new Warrior('Ork-Tisha'),
+                             new Warrior('Elf-Misha')];
+            this.health = health || getHealth(warriors);
+            this.power = power || getPower(warriors);
+        }
 
-    Kingdom.prototype.attack = function () {
-        power = Math.round(Math.random() * 50);
-        console.log("Power of kingdom %i", power);
-        return power;
-    };
+        attack() {
 
-    return this;
-}
+        }
+
+        defend(power) {
+            this.health -= power;
+            console.log("health of kingdom is %i", this.health);
+        }
+
+    }
+/* ----------------------------------- */
+
+/*--------- Класс Warrior------------- */
+    class Warrior {
+        constructor(name, health, power) {
+            this.name = name;
+            this.health = health || 100;
+            this.power = power || Math.round(Math.random() * 15);
+        }
+    }
 /* ----------------------------------- */
 
 /* ----------Класс Enemy------------- */
-function Enemy () {
-    let power;
+    class Enemy {
+        constructor(health, power) {
+            this.health = health || kingdom.health;
+            this.power = power || Math.round(Math.random() * 35);
+        }
 
-    Enemy.prototype.attack = function () {
-        power = Math.round(Math.random() * 50);
-        console.log("Power of enemy %i", power);
-        return power;
-    };
+        attack() {
 
-    return this;
-}
+        }
+
+        defend(power) {
+            this.health -= power;
+            console.log("health of enemy is %i", this.health);
+        }
+
+    }
 /* ----------------------------------- */
+
+    var kingdom = new Kingdom(),
+        battle = new Battle(),
+        enemy = new Enemy();
+
+    btnStartGame.addEventListener('click', battle.fight);
+
+};
